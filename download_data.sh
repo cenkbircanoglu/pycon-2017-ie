12dca89385c558b752ba8acfe1da20901a223d1c
Cenk Bircanoglu <cbircanoglu@boynergroup.com>
2017-10-17 18:28:44 +0300

    results of celery

--- download_data.sh
+++ download_data.sh
@@ -2,4 +2,4 @@
 
 curl -O http://ec2-54-193-19-55.us-west-1.compute.amazonaws.com/reviews.csv
 curl -O http://ec2-54-193-19-55.us-west-1.compute.amazonaws.com/ratings.csv
-curl -O http://ec2-54-193-19-55.us-west-1.compute.amazonaws.com/video_games.json
+curl -O http://ec2-54-193-19-55.us-west-1.compute.amazonaws.com/videogames.json
